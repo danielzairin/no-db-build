43f6de415a6ad729987fbc357eff32fcef8e5461
Daniel Zairin <danielzairink@gmail.com>
2025-02-24 20:33:33 +0800

    fix user-data script

--- infra/user-data.sh
+++ infra/user-data.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-# cat > index.xhtml <<EOF
-# <h1>${server_text}</h1>
-# EOF
-
-# nohup busybox httpd -f -p ${server_port} &
 sudo -i -u ubuntu bash <<EOF
   # Install NVM
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
-  \. "$HOME/.nvm/nvm.sh"
+  # Load NVM into shell
+  export NVM_DIR="/home/ubuntu/.nvm"
+  source "\$NVM_DIR/nvm.sh"
 
+  # Install Node.js
   nvm install 22
+  nvm use 22
+  nvm alias default 22
 
+  # Install dependencies and build project
   cd /home/ubuntu/
   git clone https://github.com/danielzairin/no-db-build.git
   cd no-db-build/
   npm install
   npm run build
 
-  npm start -- -H 0.0.0.0 -p 3000
+  # Start the app in the background
+  nohup npm start -- -H 0.0.0.0 -p 3000 > /home/ubuntu/app.log 2>&1 &
 EOF
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default async function Home() {
-  return <div>Hello, world</div>;
+  return <div>{new Date().toString()}</div>;
 }
